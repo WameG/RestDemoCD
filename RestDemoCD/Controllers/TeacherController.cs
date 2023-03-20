844dd5f2f5c353b880fd3e657e7761ea70297b79
WameG <wgassama@yahoo.dk>
2023-03-20 13:10:29 +0100

    Added a teacher repository and test

---
+++ RestDemoCD/Controllers/TeacherController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestDemoCD.Models;
+using RestDemoCD.Repository;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RestDemoCD.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TeacherController : ControllerBase
+    {
+
+        private readonly TeacherRepository _teacherRepository = new TeacherRepository();
+
+        // GET: api/<TeacherController>
+        [HttpGet]
+        public IEnumerable<Teacher> Get()
+        {
+            return _teacherRepository.GetAll();
+        }
+
+        // GET api/<TeacherController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<TeacherController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<TeacherController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<TeacherController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ RestDemoCD/Repository/TeacherRepository.cs
@@ -0,0 +1,6 @@
+﻿namespace RestDemoCD.Repository
+{
+    public class TeacherRepository
+    {
+    }
+}
